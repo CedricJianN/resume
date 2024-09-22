5bf1f858930b907eba9c9f488c9740d808b893d1
CedricJianN <cedricjianmartin@gmail.com>
2024-09-22 23:47:39 +0800

    App.js

--- src/App.js
+++ src/App.js
@@ -3,22 +3,77 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<div class="resume">
+    <header>
+      <h1>CEDRIC JIAN MARTIN</h1>
+      <h2>FUTURE BACK-END DEVELOPER</h2>
+    </header>
+
+    <div class="content">
+      <section class="sidebar">
+        <div class="contact">
+          <h1>Contact</h1>
+          <p><strong>Email:</strong> cjnmartin@email.com</p>
+          <p><strong>Facebook:</strong> Cedric Jian Martin</p>
+          <p><strong>Location:</strong> Sto.Tomas San matias Pampanga</p>
+
+        </div>
+
+        <div class="education">
+          <h1>Education</h1>
+          <p>Bachelor of Science in Information Technology (Present)</p>
+        </div>
+
+        <div class="skills">
+          <h1>Skills</h1>
+          <ul>
+            <li>CSS</li>
+            <li>HTML</li>
+            <li>Python</li>
+            <li>MySQL</li>
+            <li>Bootstrap</li>
+            <li>JavaScript</li>
+
+          </ul>
+        </div>
+      </section>
+
+      <section class="main-content">
+        <div class="experience">
+          <h1>Experience Section</h1>
+          <h2>1st Year 2022-2023</h2>
+          <div class="yr">
+            <h4>Python and Figma</h4>
+            <p>Answer here</p>
+            <h3>Projects</h3>
+            <ul>
+              <li>.</li>
+            </ul>
+          </div>
+
+          <div class="yr">
+            <h2>2nd Year 2023-2024</h2>
+            <h4>Javascript, MySQL, HTML and CSS</h4>
+            <p>Answer here</p>
+            <h3>Projects</h3>
+            <ul>
+              <li>.</li>
+            </ul>
+          </div>
+
+          <div class="yr">
+            <h1>3rd Year 2024 (Present) </h1>
+            <h4>HTML, CSS, REACTJS, and Bootstrap</h4>
+            <p>Answer here</p>
+            <h3>Projects</h3>
+            <ul>
+              <li>.</li>
+            </ul>
+          </div>
+        </div>
+      </section>
     </div>
+  </div>
   );
 }
 
